0d326f71a6aadd3ecde73e89b7201586e44c6548
datamover <datamover@login1.cm.cluster>
2024-12-03 07:14:05 +0000

    airflow sense nextflow run and download

---
+++ custom_sensors.py
@@ -0,0 +1,23 @@
+import os
+from airflow.sensors.base import BaseSensorOperator
+from airflow.utils.decorators import apply_defaults
+
+class DirectorySensor(BaseSensorOperator):
+    @apply_defaults
+    def __init__(self, directory_path, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.directory_path = directory_path
+
+    def poke(self, context):
+        current_directories = os.listdir("/ephemeral/datamover/nextseq")
+        print(current_directories)
+        for Dir in current_directories:
+            Base = os.path.basename(Dir)
+            if os.path.exists("/tmp/airflow."+Base):
+                print("Polled "+ Base)
+            else:
+                open("/tmp/airflow."+Base,"w")
+                context['ti'].xcom_push(key='new_directory_name', value=Base)
+                return True
+        return False 
+
---
+++ sensor.airflow.py
@@ -0,0 +1,41 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from datetime import datetime, timedelta
+
+# Custom sensor imported here
+from custom_sensors import DirectorySensor
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 11, 29),
+    'retries': 0,
+    'retry_delay': timedelta(minutes=1),
+}
+    
+dag = DAG('Download_Nextseq',catchup=False, default_args=default_args, schedule=timedelta(hours=20))
+
+directory_sensor = DirectorySensor(
+    task_id='sense_nextseq_directories',
+    directory_path='/ephemeral/datamover/nextseq',
+    poke_interval=60*5,
+    timeout=19*60*60,
+    dag=dag,
+)
+
+process_directory = BashOperator(
+    retries = 8,
+    retry_delay = timedelta(minutes=30),
+    task_id='process_new_Nexseq_run',
+    #bash_command='/tmp/newrun {{ ti.xcom_pull(task_ids="sense_nextseq_directories", key="new_directory_name") }} ',
+    bash_command='~/airflow/bash/datamover_checksum_nextseq.sh {{ ti.xcom_pull(task_ids="sense_nextseq_directories", key="new_directory_name") }} ',
+    dag=dag,
+)
+
+Trigger = TriggerDagRunOperator(
+    task_id='Trigger_Sensor',
+    trigger_dag_id=dag.dag_id,
+    dag=dag,
+)
+
+directory_sensor >> process_directory >> Trigger
